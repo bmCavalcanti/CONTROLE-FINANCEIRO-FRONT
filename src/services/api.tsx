3fd35cf9439aec7e69bdb2e9ed423fd74844022a
Bianca Cavalcanti <bm_cavalcanti@hotmail.com>
2025-01-12 17:41:25 -0300

    rota

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000", // Substitua pela URL do seu backend
+    baseURL: "http://localhost:3100", // Substitua pela URL do seu backend
     headers: {
         token: "TOKENTESTE"
     }
